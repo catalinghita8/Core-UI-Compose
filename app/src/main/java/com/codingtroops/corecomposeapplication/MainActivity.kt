1b1616bfc5631d933fa14dff75c6dce9885b52da
Catalin Ghita <cghita@airtouchmedia.com>
2020-12-16 19:49:43 +0200

    Wrap composables with alignment solution

--- app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.codingtroops.corecomposeapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
@@ -24,8 +27,21 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun MainScreen() {
-    Surface(color = Color.DarkGray,
-            modifier = Modifier.fillMaxSize()) {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Surface(
+            color = Color.Magenta,
+            modifier = Modifier.wrapContentSize(
+                align = Alignment.BottomStart
+            )
+        ) {
+            Text(
+                text = "Wrapped content",
+                style = MaterialTheme.typography.h4
+            )
+        }
 
     }
 }
