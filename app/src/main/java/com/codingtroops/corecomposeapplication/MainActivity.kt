16c809ec758a15264a207ae81e3b600e88a5d39e
Catalin Ghita <cghita@airtouchmedia.com>
2020-12-16 16:19:36 +0200

    Adding surface to screen composable

---
+++ app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.codingtroops.corecomposeapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.codingtroops.corecomposeapplication.ui.CoreComposeApplicationTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Surface(color = Color.DarkGray,
+            modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainScreen()
+}
