43733744c1f055f558fd95c99e83a88ef5de102a
Catalin Ghita <cghita@airtouchmedia.com>
2020-12-17 15:29:36 +0200

    Add column nested with row solution

--- app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.codingtroops.corecomposeapplication
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,7 +11,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.codingtroops.corecomposeapplication.ui.CoreComposeApplicationTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,22 +32,27 @@ fun MainScreen() {
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            HorizontalColoredBar(Color.Red)
-            HorizontalColoredBar(Color.Magenta)
-            HorizontalColoredBar(Color.Cyan)
-            HorizontalColoredBar(Color.Yellow)
-            HorizontalColoredBar(Color.Blue)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                ColoredSquare(Color.Red)
+                ColoredSquare(Color.Magenta)
+            }
+            ColoredSquare(Color.Cyan)
+            ColoredSquare(Color.Yellow)
+            ColoredSquare(Color.Blue)
         }
     }
 }
 
 @Composable
-fun HorizontalColoredBar(color: Color) {
+fun ColoredSquare(color: Color) {
     Surface(
         color = color,
         modifier = Modifier
             .height(100.dp)
-            .width(350.dp)
+            .width(100.dp)
     ) { }
 }
 
