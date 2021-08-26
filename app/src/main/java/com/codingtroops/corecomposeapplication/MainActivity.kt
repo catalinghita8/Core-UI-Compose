342326f4d16c6cdcf81b4004626f8b2689df478f
Catalin Ghita <cghita@airtouchmedia.com>
2021-08-26 11:09:33 +0300

    Add column nested with row solution - update

--- app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codingtroops.corecomposeapplication
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
@@ -8,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
