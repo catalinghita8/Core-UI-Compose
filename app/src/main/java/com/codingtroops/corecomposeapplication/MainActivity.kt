2625f3d1013cf754ebfb0faf6b2bef17f4ae9831
Catalin Ghita <cghita@airtouchmedia.com>
2021-08-26 11:16:32 +0300

    Extract composable solution - 1.0.1 update

--- app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codingtroops.corecomposeapplication
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
@@ -11,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codingtroops.corecomposeapplication.ui.CoreComposeApplicationTheme
