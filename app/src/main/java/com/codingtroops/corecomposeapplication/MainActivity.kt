c6fe1c03b697a552625f2afda33b84bd348b01ee
Catalin Ghita <cghita@airtouchmedia.com>
2021-08-26 11:15:23 +0300

    Create a basic layout with row - 1.0.1 update

--- app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/corecomposeapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.codingtroops.corecomposeapplication
 
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -11,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codingtroops.corecomposeapplication.ui.CoreComposeApplicationTheme
